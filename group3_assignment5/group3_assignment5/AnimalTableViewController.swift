c9f25b2fc6d82637b2cdc50f01cc8a731b07c271
Denver Luckadoo <dray@wireless-10-145-32-157.public.utexas.edu>
2018-10-02 21:05:49 -0500

    Almost finished

--- group3_assignment5/group3_assignment5/AnimalTableViewController.swift
+++ group3_assignment5/group3_assignment5/AnimalTableViewController.swift
@@ -26,11 +26,15 @@ class Animal {
 
 class AnimalTableViewCell: UITableViewCell {
     @IBOutlet weak var animalImage: UIImageView!
+    @IBOutlet weak var animalName: UILabel!
+    @IBOutlet weak var animalSciName: UILabel!
+    @IBOutlet weak var animalClass: UILabel!
+    @IBOutlet weak var animalWeight: UILabel!
 }
 
 class AnimalTableViewController: UITableViewController {
     
-    var Animals = [ Animal(image:"ahaha",name:"Aha Ha",sciName:"Aha Ha",animClass:"Insecta",size:"Size") ]
+    var Animals = [ Animal(image:"ahaha",name:"Aha Ha",sciName:"Aha Ha",animClass:"Insecta",size:"0.1 g"), Animal(image:"Animal",name:"Animal",sciName:"Muppettis Drummercus",animClass:"Muppet",size:"15 kg"), Animal(image:"saiga",name:"Saiga Antellope",sciName:"Saiga Tatarica",animClass:"Mammalia",size:"26-29 kg") ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,13 +60,19 @@ class AnimalTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "AnimalCell", for: indexPath) as! AnimalTableViewCell
         
         let animal = Animals[indexPath.row]
         cell.animalImage?.image = UIImage(named: animal.image)
-
+        cell.animalName?.text = animal.name
+        cell.animalSciName?.text = animal.sciName
+        cell.animalClass?.text = animal.animClass
+        cell.animalWeight?.text = animal.size
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(200)
     }
 
     /*
