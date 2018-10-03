d8de774585bd0856debd554dc3176dce761b6bdf
Denver Luckadoo <dray@wireless-10-145-32-157.public.utexas.edu>
2018-10-02 19:22:25 -0500

    This is the first commit/push

---
+++ group3_assignment5/group3_assignment5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  group3_assignment5
+//
+//  Created by Denver Ray Luckadoo III on 10/2/18.
+//  Copyright © 2018 Group 3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
